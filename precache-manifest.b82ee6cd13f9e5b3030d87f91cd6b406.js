1b6bdcbdf49a5d641d0c42db926d28c7b2ea7119
jch1223 <ico1828@gmail.com>
2020-10-21 17:37:32 +0900

    Updates

---
+++ precache-manifest.b82ee6cd13f9e5b3030d87f91cd6b406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d59f181184981bac159d44a2322e9b2",
+    "url": "/thumbnail_creater/index.html"
+  },
+  {
+    "revision": "0bc4a63baebaeb80c6aa",
+    "url": "/thumbnail_creater/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "d2eebf896379fcd2fb8e",
+    "url": "/thumbnail_creater/static/css/main.6f035e90.chunk.css"
+  },
+  {
+    "revision": "0bc4a63baebaeb80c6aa",
+    "url": "/thumbnail_creater/static/js/2.bfefb08b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/thumbnail_creater/static/js/2.bfefb08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2eebf896379fcd2fb8e",
+    "url": "/thumbnail_creater/static/js/main.e7ef8f90.chunk.js"
+  },
+  {
+    "revision": "8f6a4e02b4ab70a6a7d1",
+    "url": "/thumbnail_creater/static/js/runtime-main.38153c69.js"
+  }
+]);
